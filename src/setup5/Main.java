fb9e51c7b95ed26c64eecfb02b23e28709588d6a
Kim-Gyuri <gyuring0203@gmail.com>
2021-07-22 03:27:22 +0900

    알고리즘: LinkedList에 있는 노드들은 x값을 기준으로 값이 작은 것들은 왼쪽, 큰 것을 오른쪽으로 나눈다. (단 x는 오른쪽 파트 어디에 놔도 상관없다.) 첫 번째 풀이방법 : 두 줄로 세우기(포인터가 4개인 경우)

---
+++ src/setup5/Main.java
@@ -0,0 +1,95 @@
+package setup5;
+
+class LinkedList {
+    Node header;
+    static class Node{
+        int data;
+        Node next = null;
+    }
+
+    public LinkedList() {
+        header = new Node();
+    }
+
+    void append(int d) {
+        Node end = new Node();
+        end.data =d;
+        Node n = header;
+        while(n.next != null) {
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+    void retrieve() {
+        Node n = header.next;
+        while(n.next != null) {
+            System.out.print(n.data +"->");
+            n = n.next;
+        }
+        System.out.println(n.data);
+    }
+
+    public Node get(int i) {
+        Node n = header;
+        for(int j=0; j<i; j++) {
+            n = n.next;
+        }
+        return n;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.append(8);
+        ll.append(5);
+        ll.append(2);
+        ll.append(7);
+        ll.append(3);
+        ll.append(4);
+        ll.retrieve();
+
+        LinkedList.Node n = Partition(ll.get(1), 5);
+        while(n.next != null) {
+            System.out.print(n.data +"->");
+            n = n.next;
+        }
+        System.out.println(n.data);
+    }
+
+    private static LinkedList.Node Partition(LinkedList.Node n, int x) {
+        LinkedList.Node s1 = null;
+        LinkedList.Node e1 = null;
+        LinkedList.Node s2 = null;
+        LinkedList.Node e2 = null;
+
+        while(n != null) {
+            LinkedList.Node next = n.next;
+            n.next = null;
+            if(n.data < x) {
+                if(s1 == null) {
+                    s1 = n;
+                    e1 = s1;
+                } else {
+                    e1.next= n;
+                    e1 = n;
+                }
+            } else{
+                if(s2 == null) {
+                    s2 = n;
+                    e2 = s2;
+                } else{
+                    e2.next = n;
+                    e2 = n;
+                }
+            }
+            n = next;
+        }
+        if(s1 == null) {
+            return s2;
+        }
+        e1.next = s2;
+        return s1;
+    }
+}
