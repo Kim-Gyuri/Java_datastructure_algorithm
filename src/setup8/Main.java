380fdecf35c0d58a0ca27795c1a5cb7c65d3ca2f
Kim-Gyuri <gyuring0203@gmail.com>
2021-07-25 23:30:20 +0900

    단방향 LinkedList 중간노드 삭제하기( 단, 첫 번째 노드가 어딨는지 모르고, 오직 삭제할 노드만 갖고 있다.)

---
+++ src/setup8/Main.java
@@ -0,0 +1,66 @@
+package setup8;
+
+class LinkedList {
+    Node header;
+
+    static class Node{
+        int data;
+        Node next = null;
+    }
+
+    LinkedList() {
+        header = new Node();
+    }
+
+    void append(int d) {
+        Node end = new Node();
+        end.data = d;
+        Node n = header;
+        while(n.next != null) {
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+    void retrieve() {
+        Node n = header.next;
+        while(n.next != null) {
+            System.out.print(n.data +"->");
+            n = n.next;
+        }
+        System.out.println(n.data);
+    }
+
+    public boolean deleteNode(Node n) {
+        if(n == null || n.next == null) {
+            return false;
+        }
+        Node next = n.next;
+        n.data = next.data;
+        n.next = next.next;
+        return true;
+    }
+
+    public Node get(int i) {
+        Node n = header;
+        for(int j=0; j<i; j++) {
+            n=n.next;
+        }
+        return n;
+    }
+}
+
+
+public class Main{
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.append(1);
+        ll.append(2);
+        ll.append(3);
+        ll.append(4);
+        ll.retrieve();
+
+        ll.deleteNode(ll.get(2));
+        ll.retrieve();
+    }
+}
